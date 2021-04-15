2f76161b90cc277691d40c0587a24216533a72a3
Dan Isla <dan.isla@gmail.com>
2021-04-15 19:26:16 +0000

    new window tray feature, swappable with xpra tray

--- html5/js/Utilities.js
+++ html5/js/Utilities.js
@@ -547,7 +547,7 @@ const Utilities = {
 		return params;
 	},
 
-	getparam : function(prop) {
+	getparam : function(prop, withprefix) {
 		let getParameter = window.location.getParameter;
 		if (!getParameter) {
 			getParameter = function(key) {
@@ -570,8 +570,16 @@ const Utilities = {
 		}
 		value = getParameter(prop);
 		try {
-			if (value === undefined && typeof(sessionStorage) !== undefined) {
-				value = sessionStorage.getItem(prop);
+			if (value === undefined && typeof(localStorage) !== undefined) {
+				var key = prop;
+				if (withprefix === true) {
+					var prefix = location.pathname.replaceAll("/","");
+					if (prefix === "index.html") {
+						prefix = "";
+					}
+					key = prefix + "_" + prop;
+				}
+				value = localStorage.getItem(key);
 			}
 		}
 		catch (e) {
@@ -597,6 +605,21 @@ const Utilities = {
 			const key = "just for testing sessionStorage support";
 		    sessionStorage.setItem(key, "store-whatever");
 		    sessionStorage.removeItem(key);
+		    return true;
+		}
+		catch (e) {
+			return false;
+		}
+	},
+
+	hasLocalStorage : function() {
+		if (typeof(Storage) === "undefined") {
+			return false;
+		}
+		try {
+			const key = "just for testing localStorage support";
+		    localStorage.setItem(key, "store-whatever");
+		    localStorage.removeItem(key);
 		    return true;
 		}
 		catch (e) {
